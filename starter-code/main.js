61fd98f33e6be50bb28f5ebdc02d72e037905cdd
Duyen Ho <duyenho@Khanhs-MacBook-Air.local>
2016-09-22 16:14:16 +1000

    added if/else statements to compare cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,18 @@
+console.log("JS file is connected to HTML! Woo!");
+
 var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
+var cardTwo = "king";
+var cardThree = "queen";
 var cardFour = "king";
 
+if (cardTwo === cardFour) {             // compare two kings
+  alert("You have a match!");
+} else if (cardOne === cardThree) {     // compare two queens
+  alert("You have a match!");
+} else if (cardOne === cardTwo) {       // compare one of each
+  alert("Sorry, try again");
+} else if (cardOne === cardFour) {
+  alert("Sorry, try again");
+} else if (cardTwo === cardThree) {
+  alert("Sorry, try again");
+}
