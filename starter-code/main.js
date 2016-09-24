4bb07fa8dd7abd58a28af85b16cdf9e7a64907bd
Duyen Ho <duyenho@Khanhs-MacBook-Air.local>
2016-09-25 00:35:19 +1000

    created cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,14 +5,28 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {             // compare two kings
-  alert("You have a match!");
-} else if (cardOne === cardThree) {     // compare two queens
-  alert("You have a match!");
-} else if (cardOne === cardTwo) {       // compare one of each
-  alert("Sorry, try again");
-} else if (cardOne === cardFour) {
-  alert("Sorry, try again");
-} else if (cardTwo === cardThree) {
-  alert("Sorry, try again");
+// if (cardTwo === cardFour) {             // compare two kings
+//   alert("You have a match!");
+// } else if (cardOne === cardThree) {     // compare two queens
+//   alert("You have a match!");
+// } else if (cardOne === cardTwo) {       // compare one of each
+//   alert("Sorry, try again");
+// } else if (cardOne === cardFour) {
+//   alert("Sorry, try again");
+// } else if (cardTwo === cardThree) {
+//   alert("Sorry, try again");
+// }
+
+var gameBoard = document.getElementById('game-board');
+
+var cards = 4;
+
+var createCards = function() {
+  for (var i = 0; i < cards; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    gameBoard.appendChild(newCard);
+  }
 }
+
+createCards();
